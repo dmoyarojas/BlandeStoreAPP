bb25edc8fca62a615fd5083f83d98c003121bcff
David Moya <U22323683@utp.edu.pe>
2025-10-19 19:39:01 -0500

    casiii

--- BLANDE STORE CON SPRINGversion0.2/demo/start.sh
+++ BLANDE STORE CON SPRINGversion0.2/demo/start.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-# 1. Navega al directorio donde se encuentra el JAR.
-# Usa comillas dobles para manejar los espacios en los nombres de las carpetas.
-cd "BLANDE STORE CON SPRINGversion0.2/demo"
-
-# 2. Ejecuta el JAR desde esa nueva ubicación (la ruta ahora es simple: target/...).
-# El nombre de tu JAR debe ser verificado.
+# Ejecuta el JAR.
+# El JAR está en target/ dentro de la carpeta actual (/demo/).
 java -jar target/demo-0.0.1-SNAPSHOT.jar
