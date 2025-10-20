9f2e3422c90b5bbba098ec726841cc4580b51951
David Moya <U22323683@utp.edu.pe>
2025-10-19 19:47:39 -0500

    REmplazo

--- BLANDE-STORE-SPRING-V02/demo/start.sh
+++ BLANDE-STORE-SPRING-V02/demo/start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-# Ejecuta el JAR.
-# El JAR está en target/ dentro de la carpeta actual (/demo/).
+# Ejecuta el JAR. El JAR está en target/ dentro de la carpeta actual (/demo/).
 java -jar target/demo-0.0.1-SNAPSHOT.jar
